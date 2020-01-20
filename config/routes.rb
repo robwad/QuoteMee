825169f53557fb8b9dd79020ef930d8b693f77c4
robwad <darleywaddilove@college.harvard.edu>
2020-01-20 11:57:44 -0800

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :quotes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/', to: 'authors#index'
   get '/authors', to: 'authors#index'
   get '/authors/new', to: 'authors#new', as: 'new_author'
   get '/authors/:slug', to: 'authors#show'
